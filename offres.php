cb6eecba2225c150e8b36eec7e37a1b4920d63c1
Alexhuang03 <alexhuang392@yahoo.com>
2024-06-02 01:00:34 +0200

    nos offres mise en page avec abonnements & boutons-Alex

--- offres.php
+++ offres.php
@@ -1,1 +1,71 @@
 <?php
+session_start();
+if (!isset($_SESSION['user_role'])) {
+    $_SESSION['user_role'] = '';
+}
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Nos offres</title>
+
+        <style type= "text/css">
+        .profit{
+            background-color: white;
+            color: rgb(125,125,229);
+            border-color: rgb(125,125,229);
+            border-radius: 10px;
+            height: 36px;
+            width: 150px;
+            cursor: pointer;
+            border-style: solid; /*couleur unie sur la bordure*/
+            border-width: 2px;/*épaisseur de la bordure*/
+        }
+        </style>
+
+    </head>
+    <body>
+    <div id="wapper">
+
+        <?php include 'src_header.php'; ?>
+        <?php include 'src_navigation.php'; ?>
+
+        <div id="abonnement">
+            <h1>Les différents types d'abonnement</h1>
+
+            <div id="mensuel">
+                <h2>Abonnement mensuel (34.99$ par mois )</h2>
+                <h4>Présentation des avantages: </h4>
+                <ul>
+                    <li>Accès aux 100 clubs </li>
+                    <li>Caching digital inclus </li>
+                </ul>
+                <!--TO DO: bouton pr dire que j'suis intéressé, si pas connecté,-> aller à la page de connexion, sinon plateforme de payement  -->
+                <button class="profit">
+                    J'EN PROFITE
+                </button>
+            </div>
+
+            <div id="annuel">
+                <h2>Abonnement annuel (350.88$ l'année)</h2>
+                <h4>Présentation des avantages: </h4>
+                <ul>
+                    <li>Accès aux 100 clubs </li>
+                    <li>Caching digital inclus </li>
+                    <li>Carte Duo</li>
+                    <li>Pauses Vacances</li>
+                    <li>Group training</li>
+                </ul>
+                <!--TO DO: bouton pr dire que j'suis intéressé, si pas connecté,-> aller à la page de connexion, sinon plateforme de payement -->
+                <!--plateforme de payement avec le nom, prénom, adresse, IBAN -->
+                <button class="profit">
+                    J'EN PROFITE
+                </button>
+            </div>
+        </div>
+        <div id="produit"></div>
+        <?php include 'src_footer.php'; ?>
+    </body>
+</html>
