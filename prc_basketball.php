703dba4c58d80395c6740e1cc239cd14c0acea49
Edanur <edanur.rodrigues@edu.ece.fr>
2024-06-02 03:08:47 +0200

    ajout contenu + photos

--- prc_basketball.php
+++ prc_basketball.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title id="result-66572a02a4ee5">Sportify</title>
+    <title>Sportify</title>
     <link rel="stylesheet" href="PARCOURIR.css">
 </head>
 <body>
@@ -16,8 +16,8 @@
         <h2>Nos Sports de Comp&eacute;tition</h2>
         <p>Engagez-vous dans nos sports de comp&eacute;tition et am&eacute;liorez vos comp&eacute;tences. Chez Sportify, nous proposons une vari&eacute;t&eacute; de sports de comp&eacute;tition pour les athl&egrave;tes de tous niveaux. Que vous soyez un d&eacute;butant ou un comp&eacute;titeur exp&eacute;riment&eacute;, nos programmes de sports de comp&eacute;tition sont con&ccedil;us pour am&eacute;liorer vos comp&eacute;tences, renforcer votre esprit d'&eacute;quipe et vous permettre de vous d&eacute;passer.</p>
         <h2>Basketball</h2>
-        <p id="result-665ae45e2bbfd">Rejoignez nos &eacute;quipes de basketball pour des matchs et des entra&icirc;nements r&eacute;guliers. Le basketball est un sport d'&eacute;quipe passionnant qui am&eacute;liore la condition physique, la coordination, et l'esprit d'&eacute;quipe. Nos programmes sont con&ccedil;us pour tous les niveaux, que vous soyez d&eacute;butant ou joueur confirm&eacute;.</p>
-        <p><strong id="result-665913ea73591">Notre &eacute;quipe de basketball de l'ECE est fi&egrave;re d'&ecirc;tre championne de France universitaire, un titre qui t&eacute;moigne de notre engagement et de notre excellence dans ce sport.</strong></p>
+        <p>Rejoignez nos &eacute;quipes de basketball pour des matchs et des entra&icirc;nements r&eacute;guliers. Le basketball est un sport d'&eacute;quipe passionnant qui am&eacute;liore la condition physique, la coordination, et l'esprit d'&eacute;quipe. Nos programmes sont con&ccedil;us pour tous les niveaux, que vous soyez d&eacute;butant ou joueur confirm&eacute;.</p>
+        <p><strong>Notre &eacute;quipe de basketball de l'ECE est fi&egrave;re d'&ecirc;tre championne de France universitaire, un titre qui t&eacute;moigne de notre engagement et de notre excellence dans ce sport.</strong></p>
         <br>
         <figure style="text-align: center;">
             <img src="img/basket2.jpg" alt="Basketball" style="width:100%; max-width:600px; display:block; margin: 20px auto;">
@@ -26,9 +26,9 @@
         <br>
         <h3>Pourquoi Choisir le Basketball ?</h3>
         <ul>
-            <li id="result-665ae45e2bd93"><strong>Am&eacute;lioration de la Condition Physique :</strong> Le basketball est un excellent moyen d'am&eacute;liorer votre endurance, votre force et votre agilit&eacute;.</li>
-            <li id="result-665ae45e2be73"><strong>D&eacute;veloppement de Comp&eacute;tences :</strong> En jouant au basketball, vous d&eacute;veloppez des comp&eacute;tences essentielles telles que la coordination, la concentration et l'esprit d'&eacute;quipe.</li>
-            <li id="result-665ae45e2bf39"><strong>Esprit d'&Eacute;quipe :</strong> Le basketball est un sport collectif qui favorise la camaraderie et l'esprit d'&eacute;quipe, vous aidant &agrave; cr&eacute;er des liens forts avec vos co&eacute;quipiers.</li>
+            <li><strong>Am&eacute;lioration de la Condition Physique :</strong> Le basketball est un excellent moyen d'am&eacute;liorer votre endurance, votre force et votre agilit&eacute;.</li>
+            <li><strong>D&eacute;veloppement de Comp&eacute;tences :</strong> En jouant au basketball, vous d&eacute;veloppez des comp&eacute;tences essentielles telles que la coordination, la concentration et l'esprit d'&eacute;quipe.</li>
+            <li><strong>Esprit d'&Eacute;quipe :</strong> Le basketball est un sport collectif qui favorise la camaraderie et l'esprit d'&eacute;quipe, vous aidant &agrave; cr&eacute;er des liens forts avec vos co&eacute;quipiers.</li>
             <li><strong>Comp&eacute;tition et Divertissement :</strong> Participez &agrave; des matchs et des comp&eacute;titions excitantes tout en vous amusant et en vous d&eacute;fiant.</li>
             <li><strong>Encadrement de Qualit&eacute; :</strong> Nos entra&icirc;neurs exp&eacute;riment&eacute;s vous guideront pour am&eacute;liorer vos comp&eacute;tences et atteindre vos objectifs.</li>
         </ul>
@@ -38,7 +38,7 @@
             <figcaption>L'ECE champion de France Universitaire</figcaption>
         </figure>
         <br>
-        <h3>D&eacute;couvrez notre coach sp&eacute;cialis&eacute; en Basketball: </h3>
+        <h3>D&eacute;couvrez notre coach sp&eacute;cialis&eacute; en Basketball : </h3>
         <br>
         <?php $_SESSION['sport'] = "Basketball";
         include 'PARCOURIR_affiche_coach.php';
--- prc_football.php
+++ prc_football.php
@@ -16,7 +16,30 @@
 
     <section id="Football">
         <h2>Football</h2>
-        <p id="result-665889d30d223">Participez &agrave; nos tournois de football et am&eacute;liorez vos techniques de jeu.</p>
+        <p><strong> Rejoignez nos équipes de football et vivez l'excitation des matchs de compétition !</strong></p>
+        <p>Le football est bien plus qu'un sport; c'est une passion qui rassemble des millions de personnes à travers le monde. Chez Sportify, nous offrons à nos membres la possibilité de participer à des compétitions de football excitantes et de vivre des moments inoubliables sur le terrain. Participez &agrave; nos tournois de football et am&eacute;liorez vos techniques de jeu.</p>
+        <br>
+        <figure style="text-align: center;">
+            <img src="img/football2.jpg" alt="Football" style="width:100%; max-width:600px; display:block; margin: 20px auto;">
+            <figcaption>Match intense entre les équipes de football de l'ECE et de l'IESEG</figcaption>
+        </figure>
+        <br>
+        <h3>Pourquoi choisir le football de compétition avec Sportify ?</h3>
+        <ul>
+            <li><strong>Développement des Compétences :</strong> Améliorez vos compétences techniques, tactiques et physiques grâce à des entraînements intensifs et des matchs réguliers.</li>
+            <li><strong>Esprit d'Équipe :</strong>Faites partie d'une équipe soudée, où l'entraide et la camaraderie sont au cœur de l'expérience sportive.</li>
+            <li><strong>Compétitions Régulières :</strong>Participez à des tournois et championnats organisés, et mesurez-vous aux meilleures équipes locales et régionales.</li>
+            <li><strong>Encadrement Professionnel :</strong>Bénéficiez des conseils et de l'expertise de nos coachs qualifiés, qui vous aideront à atteindre vos objectifs sportifs.</li>
+            <li><strong>Événements Spéciaux :</strong>Participez à des événements spéciaux et des matchs amicaux contre d'autres clubs, et vivez des expériences uniques.</li>
+        </ul>
+        <br>
+        <figure style="text-align: center;">
+            <img src="img/football3.jpg" alt="Football" style="width:100%; max-width:600px; display:block; margin: 20px auto;">
+            <figcaption>L'équipe de football de l'ECE se prépare pour un match décisif.</figcaption>
+        </figure>
+        <br>
+            <h3>D&eacute;couvrez notre coach sp&eacute;cialis&eacute; en Football : </h3>
+        <br>
         <?php $_SESSION['sport'] = "Football";
         include 'PARCOURIR_affiche_coach.php';
         ?>
